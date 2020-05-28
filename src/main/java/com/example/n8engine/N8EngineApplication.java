b8dac456161a7c816fed14ff2995bd1a625750ac
Mihai <mihai-cristian.fagadau@my.fmi.unibuc.ro>
2020-05-28 13:59:52 +0300

    Initial commit

---
+++ src/main/java/com/example/n8engine/N8EngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.n8engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N8EngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N8EngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/n8engine/N8EngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.n8engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N8EngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
