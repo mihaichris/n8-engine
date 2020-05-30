a3481e14bbf84ea9631f41a783874b6d793ec19f
Mihai <mihai-cristian.fagadau@my.fmi.unibuc.ro>
2020-05-30 23:19:03 +0300

    added first controllers

--- src/main/java/com/example/n8engine/controller/SearchController.java
+++ src/main/java/com/example/n8engine/controller/SearchController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/home")
-public class HomeController {
+@RequestMapping("/api/search")
+public class SearchController {
 }
---
+++ src/main/java/com/example/n8engine/controller/SuggestController.java
@@ -0,0 +1,9 @@
+package com.example.n8engine.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/suggest")
+public class SuggestController {
+}
